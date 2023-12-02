686ddc9dd3b502e3fcadb822c5921ab2d6077225
Paul Bagot <bagotpa@gmail.com>
2023-12-02 10:15:30 +0100

    day2 silver star

--- day2/main.js
+++ day2/main.js
@@ -8,11 +8,38 @@ let args = content.split("\n");
  * changer le path dans la fonction readFileSync
  * 
  */
+let sum = 0;
+
+function checkColor(num, color) {
+	let Num = Number.parseInt(num);
+	if(color.includes('blue'))
+		return Num <= 14
+	if(color.includes('red'))
+		return Num <= 12
+	if(color.includes('green'))
+		return Num <= 13
+	console.log(Num + ' ' + color + ' wth')
+}
 
 
 //fonction principale
 args.forEach(line => {
+	let Args = line.split(':');
+	let iterations = Args[1].split(';');
+	let error = false;
+	iterations.forEach(iteration => {
+		colors = iteration.split(',');
+		colors.forEach(color => {
+			let parameters = color.split(' ');
+			if(!checkColor(parameters[1], parameters[2]))
+				error = true;
+		});
+	});
+	if(!error) {
+		//console.log(Args[0].split(' ')[1])
+		sum += Number.parseInt(Args[0].split(' ')[1]);
+	}
 });
 
 
-console.log("resultat");
+console.log(sum);
