7fb4545ed11f5fe02ee11cad6016dc21260251ad
Paul Bagot <bagotpa@gmail.com>
2023-12-04 07:39:44 +0100

    started day4

--- day4/main.js
+++ day4/main.js
@@ -9,11 +9,28 @@ let args = content.split("\n");
  * 
  */
 
-
+let total = 0;
 //fonction principale
 args.forEach(line => {
-	
+	let Args = line.split(':');
+	let game = Args[1].split('|');
+	let win = game[0].split(' ');
+	let numbers = game[1].split(' ');
+	let sum = 0;
+	console.log(win)
+	console.log(numbers)
+	for(let  i = 0; i < win.length; i++) {
+		for(let j = 0; j < numbers.length; j++) {
+			if(win[i] == numbers[j] && (win[i] != '' || numbers[j] != '')) {
+				console.log(win[i] + ' : ' + numbers[j])
+				if(sum == 0) sum++
+				else sum *= 2;
+				console.log(sum)
+			}
+		}
+	}
+	total += sum;
 });
 
 
-console.log("resultat");
+console.log(total);
