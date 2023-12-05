1a939dd9db5b2eeae6e10cddf976c617fd217840
Paul Bagot <bagotpa@gmail.com>
2023-12-05 20:43:50 +0100

    day5 silver star

--- day5/main.js
+++ day5/main.js
@@ -12,25 +12,51 @@ let args = content.split("\n");
 
 let index = -1;
 let seeds = [];
-seeds[0] = args[0].split(':')[1].trim().split(' ');
-let Args = [];
+seeds[0] = [];
+let cpt = 0;
+let Args = args[0].split(':')[1].trim().split(' '); 
+for(let i = 0; i < Args.length; i += 2) {
+	for(let j = 0; j < Args[i + 1]; j++) {
+		seeds[0][cpt] = parseInt(Args[i]) + j;
+		cpt++;
+	}
+}
+
+firstValue = true;
 //fonction principale
 for(let i = 1; i < args.length; i++) {
 	if(args[i].charCodeAt(0) >= 48 && args[i].charCodeAt(0) <= 57) {
+		let newValues = [];
 		let Args = args[i].trim().split(' ');
-		console.table(Args);
-		console.table(seeds[index])
+		Args[0] = parseInt(Args[0]);
+		Args[1] = parseInt(Args[1]);
+		Args[2] = parseInt(Args[2]);
 		for(let i = 0; i < seeds[index].length; i++) {
-			if(parseInt(seeds[index][i]) >= parseInt(Args[0]) && parseInt(seeds[index][i]) < parseInt(Args[2]) + parseInt(Args[1])) { // => refaire ça mdrr
-				//seeds[index][i + 1] = parseInt(seeds[index][i]) +  parseInt(Args[j][0]) -  parseInt(Args[j][1]);
-				console.log('seed : ' + seeds[index][i] + ' mini : ' + Args[0] + ' max : ' + + (parseInt(Args[0]) + parseInt(Args[2])));
-				console.log(parseInt(seeds[index][i]) - parseInt(Args[0]) + parseInt(Args[1]))
+			seeds[index][i] = parseInt(seeds[index][i]);
+			let seed = seeds[index][i];
+			if(seed >= Args[1] && seed < (Args[2] + Args[1])) {
+				newValues[i] = seed +  Args[0] -  Args[1];
+			} else if(firstValue) {
+				newValues[i] = seed;
+			} else {
+				newValues[i] = seeds[index + 1][i];
 			}
 		}
+		seeds[index + 1] = newValues;
+		newValues = null;
+		firstValue = false;
 	} else if(args[i].charCodeAt(0) >= 97 && args[i].charCodeAt(0) <= 122){
 		index++;
+		firstValue = true;
 	}
 }
 
 
-console.table(seeds);
+let min = seeds[index + 1][0];
+for(let i = 0; i < seeds[index].length; i++) {
+	if(min > seeds[index + 1][i]) min = seeds[index + 1][i];
+}
+//484023871
+//859731507
+//93788767
+console.table(min);
