4d026f7bb3e4e178fe1bcd144cb00c9c57a2fde8
Paul Bagot <bagotpa@gmail.com>
2023-12-06 01:16:22 +0100

    setup day 6

---
+++ day6/main.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const content = fs.readFileSync('./day6/input', { encoding: 'utf8', flag: 'r' });
+let args = content.split("\n");
+
+/**
+ * changer le path dans la fonction readFileSync
+ * 
+ */
+
+
+//fonction principale
+args.forEach(line => {
+
+});
+
+
+console.log("resultat");
