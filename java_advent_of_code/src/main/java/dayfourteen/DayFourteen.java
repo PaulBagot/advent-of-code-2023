8ef1925b58ef9bc73d1d3d31d13fda3b2f55fb22
Paul Bagot <bagotpa@gmail.com>
2023-12-15 14:56:42 +0100

    day14 silver star

---
+++ java_advent_of_code/src/main/java/dayfourteen/DayFourteen.java
@@ -0,0 +1,82 @@
+package main.java.dayfourteen;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.daythirteen.Mirror;
+
+public class DayFourteen {
+
+	private String fileName;
+	private List<char[]> rocks;
+	
+	public DayFourteen(String fileName) {
+		this.fileName = fileName;
+		rocks = new ArrayList<char[]>();
+	}
+	
+	public void readFile() {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+			String line;
+			while((line = reader.readLine()) != null) {
+				rocks.add(line.toCharArray());
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean toNorthOnetime() {
+		boolean mooved = false;
+		for(int i = 1; i < rocks.size(); i++) {
+			for(int j = 0; j < rocks.get(i).length; j++) {
+				if(rocks.get(i)[j] == 'O' && rocks.get(i - 1)[j] == '.') {
+					char[] line = rocks.get(i - 1);
+					line[j] = 'O';
+					rocks.set(i - 1, line);
+					line = rocks.get(i);
+					line[j] = '.';
+					rocks.set(i, line);
+					mooved = true;
+				}
+			}
+		}
+		return mooved;
+	}
+	
+	public int solve() {
+		while(toNorthOnetime());
+		int sum = 0;
+		int index = rocks.size();
+		for(char[] line : rocks) {
+			for(int i = 0; i < line.length; i++) {
+				if(line[i] == 'O')
+					sum += index;
+			}
+			index--;
+		}
+		return sum;
+	}
+	
+	
+	public static void main(String[] args) {
+		DayFourteen d = new DayFourteen("./src/main/resources/input14");
+		d.readFile();
+		for(char[] rock : d.rocks)
+			System.out.println(rock);
+		System.out.println();
+		System.out.println(d.solve());
+		for(char[] rock : d.rocks)
+			System.out.println(rock);
+	}
+	
+	
+}
